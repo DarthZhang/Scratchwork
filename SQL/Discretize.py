f47890442cf664f4ffa41179fa6d4b38a422a1ae
Andy <af1tang2@gmail.com>
2017-01-04 16:25:22 -0500

    Update Discretize.py

--- SQL/Discretize.py
+++ SQL/Discretize.py
@@ -19,14 +19,16 @@ import numpy as np
 import math
 import datetime
 import matplotlib.pyplot as plt
-from scipy import stats
+import re
 
+from scipy import stats
 from collections import Counter
 from itertools import combinations
 from datetime import date
 from datetime import time
 from datetime import timedelta
 from tempfile import mkdtemp
+from sklearn import preprocessing
 
 
 admissions_doc = '/mnt/research/data/MIMIC3/physionet.org/works/MIMICIIIClinicalDatabase/files/version_1_3/ADMISSIONS.csv.gz'
@@ -44,12 +46,13 @@ class Lab:
     
     count = 0
     
-    def __init__(df):
-        self.values = list(df['VALUE'])
-        self.flags = list(df['FLAG'])
-        self.hadm = list(df['HADM_ID'])
-        self.subj = list(df['SUBJECT_ID'])
-        self.t = list(df['TIME'])
+    def __init__(self, df):
+        #self.values = list(df['VALUE'])
+        #self.flags = list(df['FLAG'])
+        #self.hadm = list(df['HADM_ID'])
+        #self.subj = list(df['SUBJECT_ID'])
+        #self.t = list(df['TIME'])
+        self.df = df
         
     def is_number(s):
         try:
@@ -59,8 +62,8 @@ class Lab:
             return False
         
     def is_discrete (self):
-        self.unique = list(set(self.values))
-        digits = [x for x in self.unique if is_number(x)]
+        self.unique = list(set(list(self.df['VALUE'])))
+        digits = [x for x in self.unique if Lab.is_number(x)]
         if len(digits)/len(self.unique) > .95: 
             self.discrete= True
         else: 
@@ -68,23 +71,21 @@ class Lab:
                                 
     
     def clean_list(self):
-        self.values = [float(x) for x in self.values if is_number(x)]
-        if self.discrete == False:
-            #regex
-        
-            c_to_d
+        if self.discrete == True:
+            Lab.c_to_d(self)
         else:
-            d_to_d
+            Lab.d_to_d(self)
         
     def c_to_d (self):
-        ranked = stats.rankdata(self.values)
-        percentiles = ranked/len(values)*100
+        self.df['VALUE'] = self.df['VALUE'].convert_objects(convert_numberic=True)
+        ranked = stats.rankdata(list(self.df['VALUE']))
+        percentiles = ranked/len(list(self.df['VALUE']))*100
         bins = [0,20,40,60,80,100]
-        self.values = np.digitize(percentiles, bins, right= True)
+        self.df['DISCRETE_VALUE']= np.digitize(percentiles, bins, right= True)
     
     def d_to_d (self):
-        
-        
+        cats = preprocessing.LabelEncoder()
+        self.df['DISCRETE_VALUE'] = cats.fit_transform(self.df['VALUE'])        
 
 
 
@@ -111,8 +112,8 @@ def discretize():
         sql = "SELECT * from UFM where FEATURE = '%s'" % (i)
         df= pd.read_sql(sql = sql, con = conn)
         lab = Lab(df)
-        
-        # do stuff
+        lab.is_discrete()
+        lab.clean_list()
    
  #Make the dataframe into SQL table:
  
@@ -148,12 +149,25 @@ if __name__ == '__main__':
     
 # SCRATCH WORK 
     
-    #df = pd.read_csv(admissions_doc)
-    #subjects = dict(Counter(df["SUBJECT_ID"])) #creates Counter for each unique subject
-    #subj = list(subjects.keys())
-    #subj = [str(i) for i in subj]
+    #loop through labs by patients
+        #df = pd.read_csv(admissions_doc)
+        #subjects = dict(Counter(df["SUBJECT_ID"])) #creates Counter for each unique subject
+        #subj = list(subjects.keys())
+        #subj = [str(i) for i in subj]
+    
+        #for s in subj:
+        #    sql = "SELECT * from UFM where SUBJECT_ID = '%s' AND TYPE = '%s'" % (s, 'l')
+        #    df = pd.read_sql_query(sql=sql, con = conn)
 
-    #for s in subj:
-    #    sql = "SELECT * from UFM where SUBJECT_ID = '%s' AND TYPE = '%s'" % (s, 'l')
-    #    df = pd.read_sql_query(sql=sql, con = conn)
+    #regex processing
+            #strings = [x for x in self.unique if not is_number(x)]
+        #self.values = [float(x) for x in self.values if is_number(x)]
+        #if self.discrete == False:
+        #    #regex
+        #    for s in strings:
+        #        r = re.findall('\d+\.\d+', s)
+        #    c_to_d
+        #else:
+        #    d_to_d
+
         
